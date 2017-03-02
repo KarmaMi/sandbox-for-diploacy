c452929b9fb9b3ec0d1f9b5ee93c383e33982569
karmami <karmami674+github@gmail.com>
2017-03-02 16:28:21 +0900

    Add react.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const merge = require('merge2')
 
 const ts = require('gulp-typescript')
 const tsify = require('tsify')
-const typedoc = require('gulp-typedoc')
 const sourcemaps = require('gulp-sourcemaps')
 
 const mocha = require('gulp-mocha')
@@ -19,7 +18,7 @@ const tsCompilerOptions = require('./src/tsconfig.json').compilerOptions
 
 // compile source files
 gulp.task('compile-src', () => {
-  const tsResult = gulp.src(['./src/**/*.ts'])
+  const tsResult = gulp.src(['./src/**/*.ts', './src/**/*.tsx'])
     .pipe(sourcemaps.init())
     .pipe(tsSourceProject(ts.reporter.defaultReporter()))
 
@@ -46,29 +45,15 @@ gulp.task('test', ['compile-src', 'compile-test'], () => {
 })
 gulp.task('watch-test', () => gulp.watch(['src/**/*.ts', 'test/**/*.ts'], ['test']))
 
-// Create a documentation
-gulp.task('docs', () => {
-  const packageOption = require('./package.json')
-  const configs = {
-    target: tsCompilerOptions.target,
-    module: tsCompilerOptions.module,
-    out: './docs',
-    includeDeclarations: false,
-    name: packageOption.name,
-    version: true
-  }
-  gulp.src(['./src/**/*.ts'], { read: false })
-    .pipe(typedoc(configs))
-})
-
 // Browserify this module
 gulp.task('browserify', () => {
-  browserify({entries: ['browser/index.ts']})
-  .plugin(tsify, tsCompilerOptions)
+  const options = tsCompilerOptions
+  browserify({entries: ['./src/index.tsx']})
+  .plugin(tsify, options)
   .bundle()
   .pipe(source('sandbox.js'))
   .pipe(buffer())
   .pipe(sourcemaps.init())
   .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('browser/'))
+  .pipe(gulp.dest('docs/'))
 })
