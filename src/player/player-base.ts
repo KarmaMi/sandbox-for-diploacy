724f5340cec7f4d4ab0b4a876a2c115c1c3652ea
karmami <karmami674+github@gmail.com>
2017-03-11 15:26:24 +0900

    Memoize in randomNeighbor

--- src/player/player-base.ts
+++ src/player/player-base.ts
@@ -80,11 +80,12 @@ export abstract class PlayerBase<Power> {
     const eForAllHolds = E(allHolds)
 
     // Initialize simulated annealing
+    const randomNeighbor = this.mkRandomNeighbor(game.board)
     /* Decide initial temprature */
     let diffSum = 0
     let num = 0
     for (let i = 0; i < 10; i++) {
-      const n = this.randomNeighbor(game.board, allHolds)
+      const n = randomNeighbor(allHolds)
       if (n) {
         diffSum = Math.abs(eForAllHolds - E(n))
         num += 1
@@ -102,7 +103,7 @@ export abstract class PlayerBase<Power> {
       iteration: Math.pow(CANDIDATES_PER_UNIT, numOfUnits),
       alpha: ALPHA,
       initialTemprature: initialTemprature,
-      randomNeighbor: orders => this.randomNeighbor(game.board, orders),
+      randomNeighbor: orders => randomNeighbor(orders),
       evaluate: (target: Set<Order<Power>>) => -E(target)
     })
 
@@ -127,182 +128,211 @@ export abstract class PlayerBase<Power> {
     return optimal
   }
 
-  private randomNeighbor (board: Board<Power>, original: Set<Order<Power>>): Set<Order<Power>> | null {
-    const $$ = new diplomacy.standardRule.Helper(board)
+  private mkRandomNeighbor (board: Board<Power>): (original: Set<Order<Power>>) => Set<Order<Power>> | null {
+    const memoForMovableLocations = new Map<diplomacy.standardRule.Unit<Power>, Set<diplomacy.standardRule.Location<Power>>>()
+    const memoForSupportable = new Map<diplomacy.standardRule.Unit<Power>, Set<diplomacy.standardRule.Order.Order<Power>>>()
+    const memoForConvoyable = new Map<diplomacy.standardRule.Unit<Power>, Set<diplomacy.standardRule.Order.Order<Power>>>()
 
-    const origArray = Array.from(original)
-    // decide an unit to be replaced
-    const index = Math.floor(Math.random() * origArray.length)
-    const target = origArray[index]
-    const unit = target.unit
+    return (original) => {
+      const $$ = new diplomacy.standardRule.Helper(board)
 
-    let orderType = OrderType.Hold
-    if (Utils.canConvoy(board.map, unit)) {
-      // Can convoy
-      switch (Math.floor(Math.random() * 3)) {
-        case 0:
-          orderType = OrderType.Move // including Hold
-          break
-        case 1:
-          orderType = OrderType.Support
-          break
-        case 2:
-          orderType = OrderType.Convoy
-          break
-      }
-    } else {
-      switch (Math.floor(Math.random() * 2)) {
-        case 0:
-          orderType = OrderType.Move // including Hold
-          break
-        case 1:
-          orderType = OrderType.Support
-          break
-      }
-    }
+      const origArray = Array.from(original)
+      // decide an unit to be replaced
+      const index = Math.floor(Math.random() * origArray.length)
+      const target = origArray[index]
+      const unit = target.unit
 
-    let arr = origArray
-    const replaceByMoveOrHold = () => {
-      const candidates = Utils.movableLocationsOf(board, unit)
-
-      if (!(target instanceof Orders.Hold)) {
-        candidates.add(unit.location)
-      }
-
-      if (target instanceof Orders.Move) {
-        candidates.delete(target.destination)
-      }
-
-      const cs = Array.from(candidates)
-
-      const cIndex = Math.floor(Math.random() * cs.length)
-
-      const loc = cs[cIndex]
-      if (loc === unit.location) {
-        // Hold
-        arr[index] = new Orders.Hold(unit)
+      let orderType = OrderType.Hold
+      if (Utils.canConvoy(board.map, unit)) {
+        // Can convoy
+        switch (Math.floor(Math.random() * 3)) {
+          case 0:
+            orderType = OrderType.Move // including Hold
+            break
+          case 1:
+            orderType = OrderType.Support
+            break
+          case 2:
+            orderType = OrderType.Convoy
+            break
+        }
       } else {
-        // Move
-        arr[index] = new Orders.Move(unit, loc)
+        switch (Math.floor(Math.random() * 2)) {
+          case 0:
+            orderType = OrderType.Move // including Hold
+            break
+          case 1:
+            orderType = OrderType.Support
+            break
+        }
       }
-      return true
-    }
-    const replaceBySupport = () => {
-      const supportable = Utils.supportableLocationsOf(board.map, unit)
-      const candidates = new Set()
-      // Units of the same power
-      original.forEach(order => {
-        if (order === target) return
 
-        if (order instanceof diplomacy.standardRule.Order.Move) {
-          if (supportable.has(order.destination)) {
-            candidates.add(new Orders.Support(unit, order))
-          }
+      let arr = origArray
+      const replaceByMoveOrHold = () => {
+        const candidates = memoForMovableLocations.get(unit) || Utils.movableLocationsOf(board, unit)
+        if (!memoForMovableLocations.has(unit)) {
+          memoForMovableLocations.set(unit, candidates)
+        }
+
+        if (!(target instanceof Orders.Hold)) {
+          candidates.add(unit.location)
+        }
+
+        if (target instanceof Orders.Move) {
+          candidates.delete(target.destination)
+        }
+
+        const cs = Array.from(candidates)
+
+        const cIndex = Math.floor(Math.random() * cs.length)
+
+        const loc = cs[cIndex]
+        if (loc === unit.location) {
+          // Hold
+          arr[index] = new Orders.Hold(unit)
         } else {
-          if (supportable.has(order.unit.location)) {
-            candidates.add(new Orders.Support(unit, new Orders.Hold(order.unit)))
-          }
+          // Move
+          arr[index] = new Orders.Move(unit, loc)
         }
-      })
-
-      // Units of the other powers
-      board.units.forEach(unit2 => {
-        if (unit2.power === this.power) return
-
-        const ls =
-          Array.from(Utils.movableLocationsOf(board, unit2)).filter(l => supportable.has(l))
-        ls.forEach(l => {
-          candidates.add(new Orders.Support(unit, new Orders.Move(unit2, l)))
-        })
-
-        if (supportable.has(unit2.location)) {
-          candidates.add(new Orders.Support(unit, new Orders.Hold(unit2)))
-        }
-      })
-
-      let cs = Array.from(candidates)
-      if (target instanceof Orders.Support) {
-        cs = cs.filter(x => {
-          (x.destination === target.destination) && (x.target.unit === target.target.unit)
-        })
-      }
-
-      if (cs.length !== 0) {
-        const cIndex = Math.floor(Math.random() * cs.length)
-        arr[index] = cs[cIndex]
         return true
-      } else {
-        return false
       }
-    }
-    const replaceByConvoy = () => {
-      const candidates = new Set()
+      const replaceBySupport = () => {
+        const supportable = Utils.supportableLocationsOf(board.map, unit)
+        const candidates = new Set()
+        // Units of the same power
+        original.forEach(order => {
+          if (order === target) return
 
-      // Units of the same power
-      original.forEach(order => {
-        if (order === target) return
-        if (order instanceof diplomacy.standardRule.Order.Move) {
-          if (
-            Utils.isMovableViaSea(board.map, order.unit.location.province, unit.location.province, board.units) &&
-            Utils.isMovableViaSea(board.map, unit.location.province, order.destination.province, board.units)
-          ) {
-            candidates.add(new Orders.Convoy(unit, order))
-          }
-        }
-      })
-
-      // Units of the other powers
-      board.units.forEach(unit2 => {
-        if (unit2.power === this.power) return
-
-        const ls =
-          Array.from(Utils.movableViaConvoyLocationsOf(board, unit2))
-        ls.forEach(l => {
-          if (
-            Utils.isMovableViaSea(board.map, unit2.location.province, unit.location.province, board.units) &&
-            Utils.isMovableViaSea(board.map, unit.location.province, l.province, board.units)
-          ) {
-            candidates.add(new Orders.Convoy(unit, new Orders.Move(unit2, l)))
+          if (order instanceof diplomacy.standardRule.Order.Move) {
+            if (supportable.has(order.destination)) {
+              candidates.add(new Orders.Support(unit, order))
+            }
+          } else {
+            if (supportable.has(order.unit.location)) {
+              candidates.add(new Orders.Support(unit, new Orders.Hold(order.unit)))
+            }
           }
         })
-      })
 
-      let cs = Array.from(candidates)
-      if (target instanceof Orders.Convoy) {
-        cs = cs.filter(x => {
-          (x.target.destination === target.target.destination) && (x.target.unit === target.target.unit)
+        // Units of the other powers
+        const f = () => {
+          const candidates = new Set()
+          board.units.forEach(unit2 => {
+            if (unit2.power === this.power) return
+
+            const ls =
+              Array.from(Utils.movableLocationsOf(board, unit2)).filter(l => supportable.has(l))
+            ls.forEach(l => {
+              candidates.add(new Orders.Support(unit, new Orders.Move(unit2, l)))
+            })
+
+            if (supportable.has(unit2.location)) {
+              candidates.add(new Orders.Support(unit, new Orders.Hold(unit2)))
+            }
+          })
+          return candidates
+        }
+
+        const cs2 = memoForSupportable.get(unit) || f()
+
+        if (!memoForSupportable.has(unit)) {
+          memoForSupportable.set(unit, cs2)
+        }
+
+        let cs = Array.from(candidates).concat(Array.from(cs2))
+        if (target instanceof Orders.Support) {
+          cs = cs.filter(x => {
+            (x.destination === target.destination) && (x.target.unit === target.target.unit)
+          })
+        }
+
+        if (cs.length !== 0) {
+          const cIndex = Math.floor(Math.random() * cs.length)
+          arr[index] = cs[cIndex]
+          return true
+        } else {
+          return false
+        }
+      }
+      const replaceByConvoy = () => {
+        const candidates = new Set()
+
+        // Units of the same power
+        original.forEach(order => {
+          if (order === target) return
+          if (order instanceof diplomacy.standardRule.Order.Move) {
+            if (
+              Utils.isMovableViaSea(board.map, order.unit.location.province, unit.location.province, board.units) &&
+              Utils.isMovableViaSea(board.map, unit.location.province, order.destination.province, board.units)
+            ) {
+              candidates.add(new Orders.Convoy(unit, order))
+            }
+          }
         })
+
+        // Units of the other powers
+        const f = () => {
+          const candidates = new Set()
+          board.units.forEach(unit2 => {
+            if (unit2.power === this.power) return
+
+            const ls =
+              Array.from(Utils.movableViaConvoyLocationsOf(board, unit2))
+            ls.forEach(l => {
+              if (
+                Utils.isMovableViaSea(board.map, unit2.location.province, unit.location.province, board.units) &&
+                Utils.isMovableViaSea(board.map, unit.location.province, l.province, board.units)
+              ) {
+                candidates.add(new Orders.Convoy(unit, new Orders.Move(unit2, l)))
+              }
+            })
+          })
+          return candidates
+        }
+
+        const cs2 = memoForConvoyable.get(unit) || f()
+
+        if (!memoForConvoyable.has(unit)) {
+          memoForConvoyable.set(unit, cs2)
+        }
+
+        let cs = Array.from(candidates).concat(Array.from(cs2))
+        if (target instanceof Orders.Convoy) {
+          cs = cs.filter(x => {
+            (x.target.destination === target.target.destination) && (x.target.unit === target.target.unit)
+          })
+        }
+
+        if (cs.length !== 0) {
+          const cIndex = Math.floor(Math.random() * cs.length)
+          arr[index] = cs[cIndex]
+          return true
+        } else {
+          return false
+        }
       }
 
-      if (cs.length !== 0) {
-        const cIndex = Math.floor(Math.random() * cs.length)
-        arr[index] = cs[cIndex]
-        return true
-      } else {
-        return false
+      switch (orderType) {
+        case OrderType.Support:
+          if (!replaceBySupport()) {
+            // If there is no valid support orders, use hold or move
+            replaceByMoveOrHold()
+          }
+          break
+        case OrderType.Convoy:
+          if (!replaceByConvoy()) {
+            // If there is no valid convoy orders, use hold or move
+            replaceByMoveOrHold()
+          }
+          break
+        case OrderType.Hold:
+        case OrderType.Move:
+          replaceByMoveOrHold()
+          break
       }
-    }
 
-    switch (orderType) {
-      case OrderType.Support:
-        if (!replaceBySupport()) {
-          // If there is no valid support orders, use hold or move
-          replaceByMoveOrHold()
-        }
-        break
-      case OrderType.Convoy:
-        if (!replaceByConvoy()) {
-          // If there is no valid convoy orders, use hold or move
-          replaceByMoveOrHold()
-        }
-        break
-      case OrderType.Hold:
-      case OrderType.Move:
-        replaceByMoveOrHold()
-        break
+      return new Set(arr)
     }
-
-    return new Set(arr)
   }
 
   private nextRetreatOrders (game: Game<Power>, callback?: (progress: number) => void): Set<Order<Power>> {
