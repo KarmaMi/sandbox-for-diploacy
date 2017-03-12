6dd947bdd6303409af60c0432a662dd92b1cacdf
karmami <karmami674+github@gmail.com>
2017-03-13 08:21:22 +0900

    Change definition of I

--- src/player/province-importance.ts
+++ src/player/province-importance.ts
@@ -4,27 +4,38 @@ export default function provinceImportance<Power>(
   iteration: number,
   map: diplomacy.standardRule.DiplomacyMap<Power>
 ): Map<diplomacy.board.Province<Power>, number> {
-  if (iteration === 0) {
-    return new Map(Array.from(map.provinces).map(province => {
-      return <[diplomacy.board.Province<Power>, number]>[province, province.isSupplyCenter ? 1 : 0]
-    }))
-  } else {
-    const prev = provinceImportance(iteration - 1, map)
+  const maxNeighbors = Math.max(...Array.from(map.provinces).map(province => {
+    const neighbors1 = map.movableProvincesOf(province, diplomacy.standardRule.MilitaryBranch.Army)
+    const neighbors2 = map.movableProvincesOf(province, diplomacy.standardRule.MilitaryBranch.Fleet)
 
-    return new Map(
-      Array.from(map.provinces).map(province => {
-        const neighbors1 = map.movableProvincesOf(province, diplomacy.standardRule.MilitaryBranch.Army)
-        const neighbors2 = map.movableProvincesOf(province, diplomacy.standardRule.MilitaryBranch.Fleet)
+    const neighbors = new Set(Array.from(neighbors1).concat(Array.from(neighbors2)))
+    return neighbors.size
+  }))
 
-        const neighbors = new Set(Array.from(neighbors1).concat(Array.from(neighbors2)))
+  function I (iteration: number): Map<diplomacy.board.Province<Power>, number> {
+    if (iteration === 0) {
+      return new Map(Array.from(map.provinces).map(province => {
+        return <[diplomacy.board.Province<Power>, number]>[province, province.isSupplyCenter ? 1 : 0]
+      }))
+    } else {
+      const prev = I(iteration - 1)
 
-        const n = Array.from(neighbors).reduce(
-          (sum, province) => sum + (prev.get(province) || 0),
-          prev.get(province) || 0
-        )
+      return new Map(
+        Array.from(map.provinces).map(province => {
+          const neighbors1 = map.movableProvincesOf(province, diplomacy.standardRule.MilitaryBranch.Army)
+          const neighbors2 = map.movableProvincesOf(province, diplomacy.standardRule.MilitaryBranch.Fleet)
 
-        return <[diplomacy.board.Province<Power>, number]>([province, n])
-      })
-    )
+          const neighbors = new Set(Array.from(neighbors1).concat(Array.from(neighbors2)))
+
+          const n = Array.from(neighbors).reduce(
+            (sum, province) => sum + (prev.get(province) || 0) / maxNeighbors,
+            prev.get(province) || 0
+          )
+
+          return <[diplomacy.board.Province<Power>, number]>([province, n])
+        })
+      )
+    }
   }
+  return I(iteration)
 }
